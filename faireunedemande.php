58fc649e301050c6c6eeaf334b46c0a2018df6c4
Guarmanda <valentin@girod.fr>
2022-02-02 20:36:41 +0100

    respect w3c

--- faireunedemande.php
+++ faireunedemande.php
@@ -2,8 +2,8 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <?php require_once 'phpCAS/connect_cas.php' ?>
     <!-- Bootstrap CSS -->
-    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="./CSS/styles.css">
     <title>Faire une demande</title>
@@ -40,15 +40,15 @@
                                 <div class="row">
                                     <div class="col-sm-6">
                                         <label for="nom"  class="form-label">Nom</label>
-                                        <input type="text" readonly class="form-control" id="nom" name="user_nom" value="">
+                                        <input type="text" readonly class="form-control" id="nom" name="user_nom" value="<?php echo  explode(".", phpCAS::getAttributes()['mail'])[1] ?>">
                                     </div>
                                     <div class="col-sm-6">
                                         <label for="prenom" class="form-label">Prénom</label>
-                                        <input type="text" readonly class="form-control" id="prenom" name="user_prenom" value="">
+                                        <input type="text" readonly class="form-control" id="prenom" name="user_prenom" value="<?php echo explode(".", phpCAS::getAttributes()['mail'])[0] ?>">
                                     </div>
                                     <div class="col-sm-12" >
                                         <label for="email" class="form-label">Email</label>
-                                        <input type="email" readonly class="form-control" id="email" name="user_mail" value="">
+                                        <input type="email" readonly class="form-control" id="email" name="user_mail" value="<?php echo phpCAS::getAttributes()['mail'] ?>">
                                     </div>
                                     <div class="form-group col-sm-12">
                                         <label for="intitule" class="form-label">Intitulé du projet</label>
@@ -77,29 +77,28 @@
                                     <div class="row col-sm-10">
                                         <label for="prisedeRDV" class="col-sm-6 col-form-label">Souhaitez-vous un rendez-vous ?</label>
                                         <div class="col-sm-4">
-                                            <select class="form-select" id="prisedeRDV" required>
-                                                <option value="O">Oui</option>
-                                                <option selected value="N">Non</option>
+                                            <select class="form-select" id="prisedeRDV" size=2 required>
+						<option selected value="N">Non</option>
+						<option value="O">Oui</option>
                                             </select>
                                         </div>
                                     </div>
                                 </div>
-                           
                         </div>
                     </div>
                     <div class="col-sm-6">
                         <h2>Visualisation des fichiers</h2>
-
-						<div include-html="./affichage_fichier.html"></div>
+			<div include-html="./affichage_fichier.html"></div>
                         <script> includeHTML(); </script>
-                        
                     </div>
                     <button class="btn btn-primary btn-lg" type="submit" name="submit" value="Submit">Envoyer</button>
                     </form>
-                </div>            
+                </div>
             </div>
         </div>
     </section>
+<script src="./JS/verif_form.js"></script>
+
 </body>
-<script> src="JS/verif_form.js"></script>
+
 </html>
