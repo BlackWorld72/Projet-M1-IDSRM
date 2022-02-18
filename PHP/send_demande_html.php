996b0f37cd2e0b6b98811137ffe5ff00333f76ab
Guarmanda <valentin@girod.fr>
2022-02-18 11:59:47 +0100

    correction de liens

--- PHP/send_demande_html.php
+++ PHP/send_demande_html.php
@@ -1,12 +1,9 @@
 <?php
     include('connect_bdd.php');
-
-
-    
     $query_projets = "INSERT INTO demande VALUES (DEFAULT, \"".$_POST['login_cas']."\", \"".$_POST["user_nom"]."\", \"".$_POST["user_prenom"]."\", \"".$_POST["user_mail"]."\", \"".$_POST["projet_equipe_recherche"]."\", \"".$_POST["ufr"]."\", \"".$_POST["projet_intitule"]."\", \"".$_POST["projet_description"]."\", \"".$_POST["projet_datelimite"]."\", \"en attente de validation\", DEFAULT, \"".date("Y-m-d")."\");";
     echo $query_projets;
     $projets = $connect->query($query_projets);
     mysqli_close($connect);
-    header('Location: '.require_once $_SERVER['DOCUMENT_ROOT'] .'/Projet-M1-IDSRM/HTML/validation.php');
+    header('Location: /Projet-M1-IDSRM/HTML/validation.php');
     exit;
 ?>
