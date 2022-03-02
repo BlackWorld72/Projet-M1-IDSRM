9a99cd37c4856c4f77dc6ddbd88ced741af8be0e
Guarmanda <valentin@girod.fr>
2022-03-02 10:01:37 +0100

    compatibilité php 7

--- HTML/header.php
+++ HTML/header.php
@@ -2,7 +2,7 @@
     <!-- Connexion CAS -->
     <?php 
         session_start();
-        //require_once $_SERVER['DOCUMENT_ROOT'] .'/Projet-M1-IDSRM/phpCAS/connect_cas.php' 
+        require_once $_SERVER['DOCUMENT_ROOT'] .'/Projet-M1-IDSRM/phpCAS/connect_cas.php'; 
         //gestion du type d'utilisateur
         include($_SERVER['DOCUMENT_ROOT'] .'/Projet-M1-IDSRM/PHP/connect_bdd.php');
         $mail = phpCAS::getAttributes()['mail'];
@@ -13,7 +13,7 @@
             $_SESSION["user_type"] = "utilisateur";
         }
         $url = "$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-        if(!str_contains($url, ucfirst($_SESSION["user_type"]))){
+        if(!strpos($url, ucfirst($_SESSION["user_type"]))){
             header('Location: /Projet-M1-IDSRM/index.php');
         }
     ?>
