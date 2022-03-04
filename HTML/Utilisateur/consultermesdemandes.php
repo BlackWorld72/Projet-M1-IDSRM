f6443ddc8bb2f4f5b01887eecfdca715766ee5ae
Guarmanda <valentin@girod.fr>
2022-03-04 10:23:06 +0100

    ajout js gestion demande pour test

--- HTML/Utilisateur/consultermesdemandes.php
+++ HTML/Utilisateur/consultermesdemandes.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <?php require_once $_SERVER['DOCUMENT_ROOT'] .'/Projet-M1-IDSRM/HTML/header.php' ?>
-
+        <script src="/Projet-M1-IDSRM/JS/gestion_demandes.js"></script>
         <title>Consulter mes demandes</title>
    </head>
     <body>
