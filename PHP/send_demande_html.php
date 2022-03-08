c25d951baf221cb70b9bfb365add7e46db37c8a4
Guarmanda <valentin@girod.fr>
2022-03-08 14:53:24 +0100

    compatibilité main

--- PHP/send_demande_html.php
+++ PHP/send_demande_html.php
@@ -1,6 +1,5 @@
 <?php
     include('connect_bdd.php');
-<<<<<<< HEAD
 
     function escape_sql_wild($s)
     /* escapes SQL pattern wildcards in s. */
@@ -38,12 +37,7 @@
     $query_projets->bind_param('ssssssssssss', $login,$nom,$prenom,$mail,$equipe_rech,$ufr,$titre,$description,$date_lim,$suivi,$date_debut,$etat);
 
     $query_projets->execute();
-=======
-    $query_projets = "INSERT INTO demande VALUES (DEFAULT, \"".$_POST['login_cas']."\", \"".$_POST["user_nom"]."\", \"".$_POST["user_prenom"]."\", \"".$_POST["user_mail"]."\", \"".$_POST["projet_equipe_recherche"]."\", \"".$_POST["ufr"]."\", \"".$_POST["projet_intitule"]."\", \"".$_POST["projet_description"]."\", \"".$_POST["projet_datelimite"]."\", \"en attente de validation\", DEFAULT, \"".date("Y-m-d")."\");";
-    echo $query_projets;
-    $projets = $connect->query($query_projets);
-    $id = $connect->insert_id;
-
+    $id = $connect->insert_id;    
     $uploads_dir = '../upload_files';
     $countfiles = count($_FILES['fichiers']['name']);
     for($i=0;$i<$countfiles;$i++){
@@ -55,8 +49,6 @@
         $query_projets = 'INSERT INTO fichier VALUES (DEFAULT, '.$id.', "'.$type.'", "'.$uploads_dir.'/'.$name.'");';
         $req = $connect->query($query_projets);
     }
-
->>>>>>> main
     mysqli_close($connect);
     header('Location: '.require_once $_SERVER['DOCUMENT_ROOT'] .'/Projet-M1-IDSRM/HTML/validation.php');
     exit;
