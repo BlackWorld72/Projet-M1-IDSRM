eaa2dd8d8e7efc4911095f87c55f9aba565fbca1
Guarmanda <valentin@girod.fr>
2022-03-08 08:50:59 +0100

    réactivation cas

--- HTML/header.php
+++ HTML/header.php
@@ -1,6 +1,6 @@
     <meta name="viewport" content="width=device-width"/>
     <!-- Connexion CAS -->
-    <?php //require_once $_SERVER['DOCUMENT_ROOT'] .'/Projet-M1-IDSRM/phpCAS/connect_cas.php' ?>
+    <?php require_once $_SERVER['DOCUMENT_ROOT'] .'/Projet-M1-IDSRM/phpCAS/connect_cas.php' ?>
     <!-- Bootstrap 5 CSS -->
     <link rel="stylesheet" href="/Projet-M1-IDSRM/CSS/bootstrap.min.css">
     <!-- Font awesome 6 -->
