456a2dacf29b187ce108953be338127881ba8f7a
BlackWorld72 <58783535+BlackWorld72@users.noreply.github.com>
2022-03-20 16:12:53 +0100

    Add files via upload

--- PHP/get_files.php
+++ PHP/get_files.php
@@ -1,8 +1,8 @@
 <?php
-    include('connect_bdd.php');
-
-    $id_demande = $_POST["id_demande"];
-    $login_cas = $_POST["login_cas"];
+	include('connect_bdd.php');
+    
+	$id_demande = $_POST['id_demande'];
+    $login_cas = $_POST['login_cas'];
 
     /* Verification de l'utilisateur - Securisation de la requete */
     $query_projets = 'SELECT login_cas FROM demande WHERE id_demande='.$id_demande.';';
@@ -16,27 +16,23 @@
     $query_projets = 'SELECT path FROM fichier WHERE id_demande='.$id_demande.';';
     $result_projets = $connect->query($query_projets);
 
-    $zip_name =  $_SERVER['DOCUMENT_ROOT'] .'/Projet-M1-IDSRM/upload_files/'.$id_demande.'.zip';
+	if (mysqli_num_rows($result_projets) == 0) {
+		echo -1;
+		exit;
+	}
+
+    $zip_name = $_SERVER['DOCUMENT_ROOT'] .'/Projet-M1-IDSRM/upload_files/fichiers'.$id_demande.'.zip';
     $zip = new ZipArchive;
-	if ($zip->open($zip_name, ZipArchive::CREATE) === TRUE) {
+	if ($zip->open($zip_name, ZipArchive::OVERWRITE) === TRUE) {
         while ($row = mysqli_fetch_array($result_projets)) {
-            $zip->addFile($row['path']);
+			$file_name = explode("/", $row['path']);
+			$length = sizeof($file_name)-1;
+            $zip->addFile($_SERVER['DOCUMENT_ROOT'] .$row['path'], $file_name[$length]);
         }
 		$zip->close();
 	}
 
-	if (file_exists($zip_name)) {
-		header('Content-Description: File Transfer');
-		header('Content-Type: application/octet-stream');
-		header('Content-Disposition: attachment; filename="'.basename($zip_name).'"');
-		header('Expires: 0');
-		header('Cache-Control: must-revalidate');
-		header('Pragma: public');
-		header('Content-Length: ' . filesize($zip_name));
-		mysqli_close($connect);
-		echo json_encode($zip_name);
-		exit;
-	}
+	echo '/Projet-M1-IDSRM/upload_files/fichiers'.$id_demande.'.zip';
     mysqli_close($connect);
     exit;
 ?>
